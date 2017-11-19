2f77288d8e34259d6fbd923cfda214d240a9b0ae
takusemba <taku1008ele@gmail.com>
2017-11-19 11:22:21 +0900

    implement grpc

--- app/src/main/java/com/takusemba/grpc/android/MainActivity.kt
+++ app/src/main/java/com/takusemba/grpc/android/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.takusemba.grpc.android
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import com.takusemba.grpc.android.protos.HelloGrpc
+import com.takusemba.grpc.android.protos.Helloworld
+import io.grpc.ManagedChannelBuilder
+
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    findViewById<Button>(R.id.say_hi).setOnClickListener {
+      Log.d("mydebug", "hi clicked")
+
+      val channel = ManagedChannelBuilder.forAddress("10.0.2.2", 50051)
+          .usePlaintext(true)
+          .build()
+
+      val stub = HelloGrpc.newBlockingStub(channel)
+
+      val request = Helloworld.HelloRequest.newBuilder()
+          .setName("takusemba")
+          .build()
+
+      val result = stub.sayHello(request).message
+
+      Log.d("mydebug", "response: " + result)
+    }
   }
 }
